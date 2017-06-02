3c9460ef94146bc1ba8434cf6c0bf07731d03b44
Zold2012 <jonathanmercer2012@yahoo.com>
2017-06-02 10:19:26 -0500

    Work on main scene
    creating more interesting and open main scene to walk around in + worked on follow cam. looks good when using controller. not good when using keyboard. also got rid of p4connect folder... AGAIN...

--- DEIOH/Assets/Scripts/PlayerCharacterStuff/BasicFollowCam.cs
+++ DEIOH/Assets/Scripts/PlayerCharacterStuff/BasicFollowCam.cs
@@ -2,19 +2,26 @@
 
 public class BasicFollowCam : MonoBehaviour
 {
-
-	public Transform followObj;
+	public float lerpSpeed = 20f;
+	public float aheadScale = 1f;
+	public CharacterMovement followObj;
 	Vector3 offset;
+
+	Vector3 foc;
 
 	// Use this for initialization
 	void Start()
 	{
-		offset = transform.position - followObj.position;
+		offset = transform.position - followObj.transform.position;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		transform.position = followObj.position + offset;
+		if (followObj.focusposition.magnitude > 0.3f)
+		{
+			foc = followObj.focusposition;
+		}
+		transform.position = Vector3.Lerp(transform.position, followObj.transform.position + foc * aheadScale + offset, lerpSpeed * Time.deltaTime);
 	}
 }
--- DEIOH/Assets/Scripts/PlayerCharacterStuff/CharacterMovement.cs
+++ DEIOH/Assets/Scripts/PlayerCharacterStuff/CharacterMovement.cs
@@ -1,34 +1,32 @@
 ﻿using UnityEngine;
 
-namespace Deioh
+public class CharacterMovement : MonoBehaviour
 {
-	public class CharacterMovement : MonoBehaviour
+	public Vector3 focusposition;
+	public Transform characterModel;
+	public float moveSpeed = 5f;
+
+	private void Update()
 	{
-		public Transform characterModel;
-		public float moveSpeed = 5f;
+		// get input
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		// normalize input
+		float inputmag = Mathf.Clamp01(input.magnitude);
+		// translate input to animation
+		float angle = Vector2.Angle(Vector2.up, input);
 
-		private void Update()
+		if (input.x < 0)
 		{
-			// get input
-			Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-			// normalize input
-			float inputmag = Mathf.Clamp01(input.magnitude);
-			// translate input to animation
-			float angle = Vector2.Angle(Vector2.up, input);
-			print(angle);
-			if (input.x < 0)
-			{
-				angle = -angle;
-			}
-			if (inputmag > 0)
-			{
-				characterModel.localRotation = Quaternion.Euler(0, angle, 0);
-
-			}
-			// apply input to movement
-
-			GetComponent<CharacterController>().SimpleMove(characterModel.forward * moveSpeed * inputmag);
+			angle = -angle;
 		}
+		if (inputmag > 0)
+		{
+			characterModel.localRotation = Quaternion.Euler(0, angle, 0);
+
+		}
+		// apply input to movement
+
+		GetComponent<CharacterController>().SimpleMove(characterModel.forward * moveSpeed * inputmag);
+		focusposition = characterModel.forward * moveSpeed * inputmag;
 	}
 }
-
