5a52cd03614051b5cd62fd15d0c3c84a8e2e3f4e
Zold2012 <jonathanmercer2012@yahoo.com>
2017-06-02 09:06:39 -0500

    Simple Camera Movement + other stuff
    added simple camera follow script, switched serialization to text from binary cause i didn't do that earlier

---
+++ DEIOH/Assets/Scripts/PlayerCharacterStuff/BasicFollowCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BasicFollowCam : MonoBehaviour
+{
+
+	public Transform followObj;
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start()
+	{
+		offset = transform.position - followObj.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = followObj.position + offset;
+	}
+}
