cbaf4d0bf4debcb982e2b000537b47b91b3a0bb5
Zold2012 <jonathanmercer2012@yahoo.com>
2017-06-09 11:50:44 -0500

    finished switch sequence systems
    just have to hook it up to appropriate things now.
    also added some more console ideas and outlined my ideas regaurding plot

--- DEIOH/Assets/Scripts/SwitchSystem.cs
+++ DEIOH/Assets/Scripts/SwitchSystem.cs
@@ -15,16 +15,14 @@ public class SwitchSystem : MonoBehaviour
 		public int[] seq;
 	}
 
+	public int seqPos;
+	public bool[] validSequences;
+
 	// Use this for initialization
 	void Start()
 	{
-
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-
+		validSequences = new bool[sequences.Length];
+		ResetSeq();
 	}
 
 	public void RecieveSignal(string signalmessage)
@@ -38,12 +36,16 @@ public class SwitchSystem : MonoBehaviour
 				StartCoroutine(playSequence(sequences[1].seq, 0.5f));
 				break;
 			case "0":
+				InputSeq(0);
 				break;
 			case "1":
+				InputSeq(1);
 				break;
 			case "2":
+				InputSeq(2);
 				break;
 			case "3":
+				InputSeq(3);
 				break;
 		}
 	}
@@ -61,5 +63,65 @@ public class SwitchSystem : MonoBehaviour
 			// wait a bit
 			yield return new WaitForSeconds(delayBetweenThings);
 		}
+	}
+
+	void ResetSeq()
+	{
+		seqPos = 0;
+		for (int i = 0; i < sequences.Length; i++)
+		{
+			validSequences[i] = true; // you are all valid...
+		}
+	}
+
+	void InputSeq(int input)
+	{
+		bool valid = false;
+
+		// check to see if input matches any of our sequences
+		for (int v = 0; v < validSequences.Length; v++)
+		{
+			if (validSequences[v])
+			{
+				// does input match corresponding int in sequence
+				if (input == sequences[v].seq[seqPos])
+				{
+					// continue to next loop, and next num
+					valid = true;
+
+					if (seqPos + 1 == sequences[v].seq.Length)
+					{
+						// YAY you did the thing
+						ResetSeq();
+						SequenceComplete(v);
+						return;
+					}
+				}
+				else
+				{
+					validSequences[v] = false; // you are not valid. i will never see you again
+				}
+			}
+
+			// nothings working, reset
+			if (v == validSequences.Length - 1)
+			{
+				if (valid) // continue to next num in seq
+				{
+					seqPos++;
+				}
+				else // start over and look for the beginning of a new seq
+				{
+					ResetSeq();
+				}
+			}
+
+		}
+
+	}
+
+	void SequenceComplete(int i)
+	{
+		print("seq" + i);
 	}
 }
