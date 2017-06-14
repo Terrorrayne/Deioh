1cd183b35388bae2985a0aa1433c0daaedb5a405
Zold2012 <jonathanmercer2012@yahoo.com>
2017-06-14 16:28:01 -0500

    More Inventory UI work
    continuing work on PlayerItemUsage.cs, making Inventory Systems functional/interactable in-game. items can now be "equipped".

--- DEIOH/Assets/Scripts/Inventory/PlayerItemUsage.cs
+++ DEIOH/Assets/Scripts/Inventory/PlayerItemUsage.cs
@@ -7,22 +7,38 @@ public class PlayerItemUsage : MonoBehaviour
 
 	// this allows player to use items in their inventory
 
-	InventoryItem PrimaryItem; // primary item - sword
-	InventoryItem SecondaryItem; // secondary item - shield - changes usage of primary in some way, or just does its own thing
-	InventoryItem UsableItem; // usable - healthpack/consumable
+	int[] equipItems = new int[3];
+	bool inventoryMenuOpen = false;
+
+	int PrimaryItem // primary item - sword
+	{ get { return equipItems[0]; } set { equipItems[0] = value; } }
+	int SecondaryItem // secondary item - shield - changes usage of primary in some way, or just does its own thing
+	{ get { return equipItems[1]; } set { equipItems[1] = value; } }
+	int UsableItem // usable - healthpack/consumable
+	{ get { return equipItems[2]; } set { equipItems[2] = value; } }
 
 	Inventory inventory;
 
 	public RectTransform itemWheel;
-	Image[] wheelImages;
-	public RectTransform equippedItems;
+	Image[] itemImages;
+	public RectTransform equipWheel;
 	Image[] equipImages;
+
+	public Text itemLabel;
+
+	int currentItemSlot = 0;
+	int currentEquipSlot = 0;
 
 	public Sprite emptyItemSprite;
 
 	void Awake()
 	{
 		inventory = GetComponent<Inventory>();
+
+		//for (int i = 0; i < equipItems.Length; i++) // only do this if want the option to not equip anything
+		//{
+		//	equipItems[i] = -1;
+		//}
 
 		bool weGood = true;
 		if (inventory == null)
@@ -35,9 +51,9 @@ public class PlayerItemUsage : MonoBehaviour
 			print("itemWheel is unassigned");
 			weGood = false;
 		}
-		if (equippedItems == null)
+		if (equipWheel == null)
 		{
-			print("epuippedItems is unassigned");
+			print("equippedWheel is unassigned");
 			weGood = false;
 		}
 
@@ -45,15 +61,21 @@ public class PlayerItemUsage : MonoBehaviour
 			return;
 
 		// need inventory, itemWheel, and equippedItems assigned for stuff to be functional
-		wheelImages = new Image[inventory.numItemSlots];
+		itemImages = new Image[inventory.numItemSlots];
 		equipImages = new Image[3];
 
-		for (int i = 0; i < wheelImages.Length; i++)
+		for (int i = 0; i < itemImages.Length; i++) // grab all images
 		{
-			wheelImages[i] = itemWheel.GetChild(i).GetComponent<Image>();
+			itemImages[i] = itemWheel.GetChild(i).GetComponent<Image>();
+		}
+		for (int i = 0; i < equipImages.Length; i++) // grab all images
+		{
+			equipImages[i] = equipWheel.GetChild(i).GetComponent<Image>();
 		}
 
 		UpdateInventoryImages();
+		itemWheel.gameObject.SetActive(false);
+		equipWheel.gameObject.SetActive(false);
 	}
 
 	// Update is called once per frame
@@ -66,9 +88,45 @@ public class PlayerItemUsage : MonoBehaviour
 
 		// alternatively, if we scroll we auto select an item, then are given the chance to equip item. by default we use it tho
 
-		if (Input.GetButtonDown("InventoryMenu"))
+		if (Input.GetButtonDown("InventoryMenu") || Input.GetKeyDown(KeyCode.Alpha2))
 		{
 			// open/close inventory menu
+			inventoryMenuOpen = !inventoryMenuOpen;
+			itemWheel.gameObject.SetActive(inventoryMenuOpen);
+			equipWheel.gameObject.SetActive(inventoryMenuOpen);
+		}
+
+		// if the menu is open, enable navigation controls
+		if (inventoryMenuOpen)
+		{
+			Vector2 scroll = Input.mouseScrollDelta;
+			scroll.y = -scroll.y;
+			if (Input.GetKeyDown(KeyCode.Alpha1))
+				scroll.x -= 1;
+			if (Input.GetKeyDown(KeyCode.Alpha3))
+				scroll.x += 1;
+
+
+			currentEquipSlot = (currentEquipSlot + (int)scroll.x) % 3;
+			if (currentEquipSlot < 0)
+				currentEquipSlot = 2;
+
+			currentItemSlot = equipItems[currentEquipSlot];
+
+			currentItemSlot = (currentItemSlot + (int)scroll.y) % 8;
+			if (currentItemSlot < 0)
+				currentItemSlot = 7;
+
+			equipItems[currentEquipSlot] = currentItemSlot;
+			UpdateEquipImages();
+
+			// todo: get label to work
+			//itemLabel.text = inventory.items[equipItems[currentEquipSlot]].displayName;
+
+			// update positions
+			itemWheel.localPosition = Vector3.up * currentItemSlot * 45;
+			equipWheel.localPosition = Vector3.left * currentEquipSlot * 60;
+
 		}
 	}
 
@@ -79,11 +137,38 @@ public class PlayerItemUsage : MonoBehaviour
 			// cycle through all items
 			if (inventory.items[i] == null)
 			{
-				wheelImages[i].sprite = emptyItemSprite;
+				itemImages[i].sprite = emptyItemSprite;
 			}
 			else
 			{
-				wheelImages[i].sprite = inventory.items[i].itemSprite;
+				itemImages[i].sprite = inventory.items[i].itemSprite;
+			}
+		}
+
+		UpdateEquipImages();
+	}
+
+	public void UpdateEquipImages()
+	{
+
+		for (int i = 0; i < equipItems.Length; i++)
+		{
+			//if (equipItems[i] == -1)
+			//{
+			//	equipImages[i].sprite = emptyItemSprite;
+			//}
+			//else
+			//{
+			//	equipImages[i].sprite = inventory.items[i].itemSprite;
+			//}
+
+			if (inventory.items[equipItems[i]] == null)
+			{
+				equipImages[i].sprite = emptyItemSprite;
+			}
+			else
+			{
+				equipImages[i].sprite = inventory.items[equipItems[i]].itemSprite;
 			}
 		}
 	}
