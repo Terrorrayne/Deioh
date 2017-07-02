0d83e9a7d653c59c0f95ac9199a95fa0fbb0035f
Terrorrayne <goggles.ab@gmail.com>
2017-07-02 13:28:17 +0800

    I don't know anymore
    Unity still hates me, but visual studio doesn't.
    
    If unity liked me again, I could do the code testing easier. Can you merge, please?

---
+++ DEIOH/EnemyAI.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float enemyLineOfSight;
+    public float enemyAggroRange;
+    public float enemyAttackRange;
+    public float enemyMovementSpeed;
+    public float damping;
+    public transform taget;
+    Rigidbody theRigidBody;
+    Renderer myRender;
+
+    //initilize
+    void Start () {
+        theRigidBody = GetComponent<>();
+        myRender = GetComponent<>();
+}
+
+    //Update
+    void FixedUpdate () {
+        enemyAggroRange = Vector3.Distance(target.position, transform.position);
+        if (enemyAggroRange<enemyLineOfSight) {
+            myRender.material.color=Color.yellow;
+            lookAtPlayer();
+            print("I'm watching you!");
+        }
+        if (enemyAggroRange<enemyAttackRange) {
+            attackPlease();
+            print ("Take this!");
+        }
+}
+
+    void LookAtPlayer ()
+    {
+        Quaternion rotation = Quaternion.LookRotation(target.position - tranform.position);
+        transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime*damping);
+    }
+    void Attack ()
+    {
+        theRigidBody.AddForce(transform.forward*enemyMovementSpeed);
+    }
+
+    public void EnemyAI.Class1()
+	{
+	}
+}
